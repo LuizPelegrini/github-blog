a85aa2a99fd5efb729ff171e6048f88538d4a6b3
Luiz Guilherme <ldesouzapelegrini01@gmail.com>
2022-12-12 15:57:12 +0800

    add: Create static profile component

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,3 +1,56 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import {
+  faBuilding,
+  faUserGroup,
+  faArrowUpRightFromSquare,
+} from '@fortawesome/free-solid-svg-icons';
+
+import {
+  ProfileContainer,
+  ProfileDetails,
+  ProfileHeader,
+  Footer,
+} from './styles';
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <ProfileContainer>
+        <img src="https://github.com/LuizPelegrini.png" alt="" />
+        <ProfileDetails>
+          <ProfileHeader>
+            <h2>Cameron Williamson</h2>
+            <a
+              href="https://github.com/LuizPelegrini"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <span>Github</span>
+              <FontAwesomeIcon icon={faArrowUpRightFromSquare} />
+            </a>
+          </ProfileHeader>
+          <p>
+            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu
+            viverra massa quam dignissim aenean malesuada suscipit. Nunc,
+            volutpat pulvinar vel mass.
+          </p>
+          <Footer>
+            <div>
+              <FontAwesomeIcon icon={faGithub} />
+              <span>cameronwll</span>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faBuilding} />
+              <span>Rocketseat</span>
+            </div>
+            <div>
+              <FontAwesomeIcon icon={faUserGroup} />
+              <span>32 followers</span>
+            </div>
+          </Footer>
+        </ProfileDetails>
+      </ProfileContainer>
+    </>
+  );
 }
